e4364b05d25ad41cb07bc3fc111e47fcb6c1bc8c
Z-BUSTER <121357436+Z-BUSTER@users.noreply.github.com>
2026-03-20 19:17:52 -0400

    changes again

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -12,6 +12,7 @@ def test_addition():
     assert simple_calculator("add", -2, 2) == 0         # Test for negative and positive number
     assert simple_calculator("add", 0, 0) == 0          # Test for zero addition
 
+
 def test_subtraction():
     assert simple_calculator("subtract", 5, 3) == 2     # Test for positive numbers
     assert simple_calculator("subtract", -2, -2) == 0   # Test for negative numbers
